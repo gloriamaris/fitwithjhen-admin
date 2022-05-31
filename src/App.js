f14e17395ea2dd4246309807704143e427f87f41
monique.dingding <monique.dingding@lamudi.com>
2022-05-31 21:31:24 +0800

    chore: add header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { Button, createTheme, ThemeProvider, Typography } from '@mui/material';
+import { Container } from '@mui/system';
+import styles from './styles';
+import Header from './components/Header';
+
+const theme = createTheme(styles);
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container 
+        disableGutters
+        maxWidth='false'>
+        <Header/>
+      </Container>
+      
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Typography variant="h1" component="h2">
+            h1. Heading
+          </Typography>
+          <Button variant="contained" disableElevation>
+            Disable elevation
+          </Button>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles.js
@@ -0,0 +1,32 @@
+const styles = {
+  status: {
+    danger: '#e53e3e',
+  },
+  palette: {
+    primary: {
+      main: '#F0DEDC',
+      darker: '#E2ACA6',
+      dark: '#E2ACA6',
+      contrastText: '#362F2E',
+    },
+    neutral: {
+      main: '#FAF2F1',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#CDF8FD',
+      main: '#FAF2F1',
+      // dark: will be calculated from palette.secondary.main,
+      contrastText: '#362F2E',
+    },
+    // Used by `getContrastText()` to maximize the contrast between
+    // the background and the text.
+    contrastThreshold: 3,
+    // Used by the functions below to shift a color's luminance by approximately
+    // two indexes within its tonal palette.
+    // E.g., shift from Red 500 to Red 300 or Red 700.
+    tonalOffset: 0.2,
+  },
+}
+
+export default styles
